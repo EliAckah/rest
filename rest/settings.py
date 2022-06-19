a0ce4db908a0513973615643605659c7deec721f
Elijah Ackah <eliackah18@gmail.com>
2022-06-19 00:05:20 +0000

    added staticfiles

--- rest/settings.py
+++ rest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4+-2+b8vz4q+$mz2vhr-_&2km!pxu*-bkspbk@0js9uwv&dgv%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://restcharityfoundation.herokuapp.com/','localhost:8000']
 
